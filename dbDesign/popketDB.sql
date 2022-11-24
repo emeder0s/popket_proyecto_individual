a5bd69d0029c0ac0c4161f9e7696639831aeefcb
Elena Mederos <elenamc91@hotmail.com>
2022-11-24 16:29:37 +0100

    Diseño de la base de datos, carpeta de inspiración y carpeta de app

--- dbDesign/popketDB.sql
+++ dbDesign/popketDB.sql
@@ -17,22 +17,70 @@ CREATE TABLE IF NOT EXISTS addresses(
 CREATE TABLE IF NOT EXISTS users(
         id INT AUTO_INCREMENT NOT NULL,
         first_name VARCHAR(25) NOT NULL, 
-        surname VARCHAR(25) NOT NULL, 
-        email VARCHAR (25) NOT NULL,
+        last_name VARCHAR(25) NOT NULL, 
+        email VARCHAR (25) UNIQUE NOT NULL,
         phone VARCHAR(15),
-        fk_id_adress INT,
+        fk_id_address INT,
         PRIMARY KEY(id),
-        FOREIGN KEY (address) REFERENCES addresses (id) ON DELETE SET NULL
+        FOREIGN KEY (fk_id_address) REFERENCES addresses(id) ON DELETE SET NULL
     );
+    
+CREATE TABLE IF NOT EXISTS spaces(
+        id INT AUTO_INCREMENT NOT NULL,
+        name_space VARCHAR(50) NOT NULL, 
+        description VARCHAR(500), 
+		state VARCHAR(6) DEFAULT "draft",
+        PRIMARY KEY(id)
+);
 
 CREATE TABLE IF NOT EXISTS spacers(
         id INT AUTO_INCREMENT NOT NULL,
         first_name VARCHAR(25) NOT NULL, 
-        surname VARCHAR(25) NOT NULL, 
-        email VARCHAR (25) NOT NULL,
+        last_name VARCHAR(25) NOT NULL, 
+        email VARCHAR (25) UNIQUE NOT NULL,
         phone VARCHAR(15),
         fk_id_address INT,
+        fk_id_space INT,
+        PRIMARY KEY(id),
+        FOREIGN KEY (fk_id_address) REFERENCES addresses(id) ON DELETE SET NULL,
+        FOREIGN KEY (fk_id_space) REFERENCES spaces(id) ON DELETE SET NULL
+);
+
+CREATE TABLE IF NOT EXISTS products(
+        id INT AUTO_INCREMENT NOT NULL,
+        product_name VARCHAR(50) NOT NULL, 
+        description VARCHAR(500), 
+        price VARCHAR (25) NOT NULL,
+        image VARCHAR(512) NOT NULL,
         fk_id_space INT NOT NULL,
         PRIMARY KEY(id),
-        FOREIGN KEY (address) REFERENCES addresses (id) ON DELETE SET NULL
-    );
+        FOREIGN KEY (fk_id_space) REFERENCES spaces(id)
+);
+
+CREATE TABLE IF NOT EXISTS orders(
+        id INT AUTO_INCREMENT NOT NULL,
+        total_account VARCHAR(25) NOT NULL, 
+        num_order VARCHAR(12) NOT NULL, 
+        pick_up_adddress VARCHAR(300), 
+        address VARCHAR(500),
+        PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS orders_products(
+        id INT AUTO_INCREMENT NOT NULL,
+        quantity INT NOT NULL,
+        fk_id_order INT NOT NULL,
+        fk_id_product INT NOT NULL,
+        PRIMARY KEY(id),
+        FOREIGN KEY (fk_id_order) REFERENCES orders(id),
+        FOREIGN KEY (fk_id_order) REFERENCES products(id)
+);
+
+CREATE TABLE IF NOT EXISTS users_spacers_orders(
+        id INT AUTO_INCREMENT NOT NULL,
+        name_table VARCHAR(25) NOT NULL, 
+        id_user_spacer INT NOT NULL,
+        fk_id_order INT NOT NULL,
+        PRIMARY KEY(id),
+        FOREIGN KEY (fk_id_order) REFERENCES orders(id)
+);
