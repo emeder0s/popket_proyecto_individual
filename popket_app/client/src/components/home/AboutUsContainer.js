d04653cf03820e302dfe582eaec6854a4d12c232
Elena Mederos <elenamc91@hotmail.com>
2022-12-10 17:18:15 +0100

    Añadido texto de about Us y estilos

--- popket_app/client/src/components/home/AboutUsContainer.js
+++ popket_app/client/src/components/home/AboutUsContainer.js
@@ -5,6 +5,19 @@ function AboutUsContainer() {
 
   return (
     <div className="about-us-container">
+      <div className="about-us-text-container">
+        <h2>About Us</h2>
+        <p>POPKET es una iniciativa para lanzar artistas, tanto digitales como antersanos.</p>
+        <p>Desde la web de POPKET, queremos que puedan darse a conocer y difundar sus creaciones, dado que el mercado online hoy en día puede llegar a casi todo las partes del mundo.</p>
+        <p>Conectamos compradores y vendedores mediante productos especiales, creativos y realizados con mucho cariño y esfuerzo.</p>
+        <p>Y sobretodo crear conexiones seguras entre ellos.</p>
+        <p>Queremos crear comunidad, empoderando a los vendedores y facilitando el proceso de compra a los compradores.</p>
+        <br></br>
+
+        <p className="sign">Firmado,</p>
+        <p className="sign">POPKET</p>
+      
+      </div>
     </div>
       
   );
