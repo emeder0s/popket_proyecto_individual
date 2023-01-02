a4532379ab3329295f3ce9a83e53c5a41b51d859
Elena Mederos <elenamc91@hotmail.com>
2023-01-02 19:18:52 +0100

    Formulario de datos personales- editar datos de usuario terminado

--- popket_app/client/src/components/account/PersonalDataForm.js
+++ popket_app/client/src/components/account/PersonalDataForm.js
@@ -1,35 +1,64 @@
-import React, { useState }  from "react";
+import React, { useEffect, useState }  from "react";
 import { Link } from 'react-router-dom';
+import { postFetch } from '../../helpers/fetchs';
 import '../../style/body.css';
 import '../../style/my-account.css';
 
-function PersonalDataForm(props) {
-    const [user,setUser]= useState();
-    // console.log(props.isSpacer)
-    // fetch("/show-user")
-    // .then((res) => res.json(res))
-    // .then(res=>{
-    //     setSpace(res);
-    // })    
+function PersonalDataForm(props) { 
+    const [user, setUser] = useState();
+    const [msn,setMsn] =  useState("");
+    
+    const getUser = async () =>{
+        var user_id = JSON.parse(localStorage.getItem("user")).id;
+        var endpoint = "";
+        if (props.isSpacer){
+            endpoint = "/spacer/"+ user_id;
+        }else{
+            endpoint = "/user/"+ user_id;
+        }
+        await fetch(endpoint)
+        .then((res) => res.json(res))
+        .then(res=>{
+            setUser(res);
+        }) 
+    }
+    useEffect(()=>{getUser()},[]);
 
 
     const editData = async e => {
-        alert("hola")
+        e.preventDefault();
+        document.getElementById("sucess-message").style.display="none";
+        var data = {first_name:e.target.first_name.value,last_name:e.target.last_name.value,email:e.target.email.value,phone:e.target.phone.value};
+        if (props.isSpacer){
+            var endpoint = "/edit-spacer/";
+        }else{
+            var endpoint = "/edit-user/";
+        }
+        await postFetch(endpoint, data)
+        .then((res) => res.json(res))
+        .then(res=>{
+            setMsn("Datos guardados correctamente");
+            document.getElementById("sucess-message").style.display="block";
+        })
     }
     
     return(
-     
         <div className="personal-data-form">
-            <h3>Datos Personales</h3>
-            <h4>Edita tus datos</h4>
-            <form onSubmit={editData}>
-                <input type="text" name='first_name' placeholder='Nombre' required></input>
-                <input type="text" name='last_name' placeholder='Apellidos' required></input>
-                <input type="email" name='email' placeholder='Email' required></input>
-                <input type="tex" name='phone' placeholder='Teléfono' required></input>
-                {/* <p id="error-message" style={{display: "none"}}>{msn}</p> */}
-                <button type="submit">Guardar</button>
-            </form>   
+            {user ? 
+            <div>
+                <h3>Datos Personales</h3>
+                <h4>Edita tus datos</h4>
+                <p id="sucess-message" style={{display: "none"}}>{msn}</p>
+                <form onSubmit={editData}>
+                    <input type="text" name='first_name' placeholder='Nombre' required defaultValue={user.first_name}></input>
+                    <input type="text" name='last_name' placeholder='Apellidos' required defaultValue={user.last_name}></input>
+                    <input type="email" name='email' placeholder='Email' required defaultValue={user.email}></input>
+                    <input type="tex" name='phone' placeholder='Teléfono' required defaultValue={user.phone}></input>
+                    <button type="submit">Guardar</button>
+                </form> 
+            </div>
+            :""}
+              
         </div>
     )
 }
--- popket_app/client/src/pages/MyAccount.js
+++ popket_app/client/src/pages/MyAccount.js
@@ -1,4 +1,4 @@
-import React, { useState }  from "react";
+import React, { useState, useEffect }  from "react";
 import { Link } from 'react-router-dom';
 import { checkAuth } from '../helpers/checkAuth';
 import PersonalDataForm from '../components/account/PersonalDataForm';
@@ -10,6 +10,7 @@ import '../style/my-account.css';
 
 function MyAccount() {
     var auth = checkAuth();
+    const [viewIsSpacer, setViewIsSpacer] = useState(false);
     const [activeContent, setActiveContent] = useState("personal-data");
     const activeLi = (optionId) => {
         var activeOne = document.getElementsByClassName("menu-option active")[0];
@@ -19,16 +20,18 @@ function MyAccount() {
               setActiveContent(optionId);
           }
     }
-
-    fetch("/is-spacer")
-    .then((res) => res.json(res))
-    .then(res=>{
-        if(res){
-            var isSpacer= true;
-        }else{
-            var isSpacer= false;
-        } 
-    });
+    useEffect(()=>{
+        const checkSpacer = async () =>{
+            await fetch("/is-spacer")
+            .then((res) => res.json(res))
+            .then(res=>{
+                if(res=={}){
+                    setViewIsSpacer(true);
+                }
+            });
+        }
+        checkSpacer();
+    },[]);
     
     return(
         <div className="page-content page-content-account">
@@ -38,11 +41,11 @@ function MyAccount() {
                    <li id="address" className="menu-option" onClick={() => activeLi("address")}>Dirección</li>
                    <li id="password" className="menu-option" onClick={() => activeLi("password")}>Contraseña</li>
                    <li id="orders" className="menu-option" onClick={() => activeLi("orders")}>Pedidos</li> 
-                   {isSpacer ? <li><Link to='/' className="nav-link">Editar Espacio</Link></li> : ""}
+                   {viewIsSpacer ? <li><Link to='/' className="nav-link">Editar Espacio</Link></li> : ""}
                 </ul>
             </div>
             <div className="sidebar-content">
-                {activeContent == "personal-data" ? <div id="personal-data-content" className="personal-data" ><PersonalDataForm isSpacer={isSpacer}></PersonalDataForm></div> : ""}
+                {activeContent == "personal-data" ? <div id="personal-data-content" className="personal-data" ><PersonalDataForm isSpacer={viewIsSpacer}></PersonalDataForm></div> : ""}
                 {activeContent == "address" ? <div id="address-content" className="address" ><AddressForm></AddressForm></div> : ""}
                 {activeContent == "password" ? <div id="password-content" className="password"><PasswordForm></PasswordForm></div> : ""}
                 {activeContent == "orders" ? <div id="orders-content"className="orders" ><Orders></Orders></div> : ""}     
